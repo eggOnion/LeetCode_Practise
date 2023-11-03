6201867a8e06de8aca0acf4156eeff2513e8a83a
Wei Cong TONG <137802716+eggOnion@users.noreply.github.com>
2023-11-03 17:17:50 +0800

    Added solution code

--- TwoSum/Solution.java
+++ TwoSum/Solution.java
@@ -1,6 +1,27 @@
-public class Solution{
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
+public class Solution{
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+              
+        int[] nums = {2,7,11,15};
+        int sum = 0;
+        int target = 26;
+
+
+        for(int i=0; i<nums.length; i++){
+            
+            if((i+1) != nums.length)        //this is a protection to prevent ArrayIndexOutOfBounds
+                sum = nums[i] + nums[i+1];
+
+            if(sum==target){
+                System.out.println("You hit the target!");
+                System.out.println("index is: [" + i + "," + (i+1)+"]");
+                break;
+            }          
+        }
+
+        if(sum != target)
+            System.out.println("You did not hit your target");
+   }
 }
