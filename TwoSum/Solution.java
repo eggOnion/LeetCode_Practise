1704897a50a66c271b288caec4f94f6d6ee19fab
Wei Cong TONG <137802716+eggOnion@users.noreply.github.com>
2023-11-03 23:39:28 +0800

    Move implementation into method

--- TwoSum/Solution.java
+++ TwoSum/Solution.java
@@ -1,9 +1,11 @@
+import java.util.Arrays;
+
 public class Solution{
-    public static void main(String[] args) {
-              
-        int[] nums = {2,7,11,15};
+
+    public static int[] twoSum(int[] nums, int target) {
+
         int sum = 0;
-        int target = 26;
+        int[] result = new int[2];
 
 
         for(int i=0; i<nums.length; i++){
@@ -12,13 +14,31 @@ public class Solution{
                 sum = nums[i] + nums[i+1];
 
             if(sum==target){
-                System.out.println("You hit the target!");
-                System.out.println("index is: [" + i + "," + (i+1)+"]");
+                result[0] = i;
+                result[1] = i+1;
                 break;
             }          
         }
 
         if(sum != target)
             System.out.println("You did not hit your target");
+        else
+            System.out.println("You hit the target! \n" + Arrays.toString(result));         
+
+        return result;
    }
+    public static void main(String[] args) {
+              
+        int[] nums = {2,7,11,15};
+        int target = 26;        
+
+        twoSum(nums, target);
+        
+
+        //Retrieve the return value from twoSum method
+        // for (int i : twoSum(nums, target)) {
+        //     System.out.println(i);
+        // }
+    }
+        
 }
