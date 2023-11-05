3e0a0566296c3049e433b256a8d15cb920260945
Wei Cong TONG <137802716+eggOnion@users.noreply.github.com>
2023-11-05 21:09:52 +0800

    code update

--- TwoSum/Solution.java
+++ TwoSum/Solution.java
@@ -5,40 +5,40 @@ public class Solution{
     public static int[] twoSum(int[] nums, int target) {
 
         int sum = 0;
+        int genesisIndex = 0;
         int[] result = new int[2];
 
 
         for(int i=0; i<nums.length; i++){
-            
+        
             if((i+1) != nums.length)        //this is a protection to prevent ArrayIndexOutOfBounds
-                sum = nums[i] + nums[i+1];
+                sum = nums[genesisIndex] + nums[i+1];            
 
             if(sum==target){
-                result[0] = i;
+                result[0] = genesisIndex;
                 result[1] = i+1;
                 break;
-            }          
-        }
+            }
 
-        if(sum != target)
-            System.out.println("You did not hit your target");
-        else
-            System.out.println("You hit the target! \n" + Arrays.toString(result));         
+            if(sum!=target && (i == nums.length-1)){   
+                
+                if(genesisIndex==nums.length-1)
+                    break;
+                
+                i=0;      
+                sum=0;       
+                genesisIndex++;
+                continue;                
+            }                     
+        }
 
         return result;
    }
     public static void main(String[] args) {
               
         int[] nums = {2,7,11,15};
-        int target = 26;        
+        int target = 17;        
 
-        twoSum(nums, target);
-        
-
-        //Retrieve the return value from twoSum method
-        // for (int i : twoSum(nums, target)) {
-        //     System.out.println(i);
-        // }
-    }
-        
+        System.out.println(Arrays.toString(twoSum(nums, target)));     
+    }        
 }
